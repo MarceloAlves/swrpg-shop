43526ae4d2b76609185794db1d1127b47278b68f
Marcelo Alves <selfmadecelo@gmail.com>
2017-07-14 19:03:42 -0700

    Cleaned up DicePool
    Clean up, clean up, everybody do your part

--- app/lib/dice_pool.rb
+++ app/lib/dice_pool.rb
@@ -1,65 +1,34 @@
 class DicePool
-  def initialize(skill_level:, characteristic_level:, boost_dice:, setback_dice:)
-    number_ability_dice = calculate_ability_dice(characteristic_level: characteristic_level, skill_level: skill_level)
+  def initialize(skill_level: 0, characteristic_level: 0, number_boost_dice: 0, number_setback_dice: 0)
+    number_ability_dice   = calculate_ability_dice(characteristic_level: characteristic_level, skill_level: skill_level)
     number_profiency_dice = calculate_profiency_dice(characteristic_level: characteristic_level, skill_level: skill_level)
-
-    @dice_count = calculate_number_of_dice(number_ability_dice, number_profiency_dice, boost_dice, setback_dice)
-    @dice_pool = Array.new(@dice_count) { Array.new(2) }
-
-    @dice_face_count = Array.new(@dice_count)
-    count = 0
-
-    (0..number_ability_dice - 1).each do |i|
-      @dice_pool[i] = Dice.ability
-      @dice_face_count[i] = Dice.ability.count
-    end
-
-    count = number_ability_dice
-
-    (count..(number_profiency_dice + count) - 1).each do |i|
-      @dice_pool[i] = Dice.proficiency
-      @dice_face_count[i] = Dice.proficiency.count
-    end
-
-    count += number_profiency_dice
-
-    (count..(boost_dice + count) - 1).each do |i|
-      @dice_pool[i] = Dice.boost
-      @dice_face_count[i] = Dice.boost.count
-    end
-
-    count += boost_dice
-
-    (count..(setback_dice + count) - 1).each do |i|
-      @dice_pool[i] = Dice.setback
-      @dice_face_count[i] = Dice.setback.count
-    end
-
-    count =+ setback_dice
+    dice_counts = {
+      ability_dice:   number_ability_dice,
+      profiency_dice: number_profiency_dice,
+      boost_dice:     number_boost_dice,
+      setback_dice:   number_setback_dice
+    }
+    @dice_pool = []
+    @dice_face_count = []
+    @dice_count = dice_counts.values.sum
+    create_dice_pool(dice_counts)
   end
 
-  def roll(item_rarity:)
-    leftover_success, leftover_advantages, leftover_triumphs = 0, 0, 0
+  def roll(item_rarity: 1)
+    leftover_success = leftover_advantages = leftover_triumphs = 0
     number_of_difficulty_dice = item_rarity / 2
 
-    (0..@dice_count - 1).each do |i|
-      leftover_success += @dice_pool[i][rand(0..@dice_face_count[i] - 1)][0]
+    @dice_count.times do |i|
+      i--
+      random_end           = @dice_face_count[i] - 1
+      leftover_success    += @dice_pool[i][rand(0..random_end)][0]
+      leftover_advantages += @dice_pool[i][rand(0..random_end)][1]
+      leftover_triumphs   += @dice_pool[i][rand(0..random_end)][2]
     end
 
-    (0..number_of_difficulty_dice - 1).each do |i|
-      leftover_success += Dice.difficulty[rand(0..Dice.difficulty.count - 1)][0]
-    end
-
-    (0..@dice_count - 1).each do |i|
-      leftover_advantages += @dice_pool[i][rand(0..@dice_face_count[i] - 1)][1]
-    end
-
-    (0..number_of_difficulty_dice - 1).each do |i|
+    number_of_difficulty_dice.times do
+      leftover_success    += Dice.difficulty[rand(0..Dice.difficulty.count - 1)][0]
       leftover_advantages += Dice.difficulty[rand(0..Dice.difficulty.count - 1)][1]
-    end
-
-    (0..@dice_count - 1).each do |i|
-      leftover_triumphs += @dice_pool[i][rand(0..@dice_face_count[i] - 1)][2]
     end
 
     [leftover_success, leftover_advantages, leftover_triumphs]
@@ -75,7 +44,25 @@ class DicePool
     characteristic_level >= skill_level ? skill_level : characteristic_level
   end
 
-  def calculate_number_of_dice(ability_dice, profiency_dice, boost_dice, setback_dice)
-    ability_dice + profiency_dice + boost_dice + setback_dice
+  def create_dice_pool(ability_dice:, profiency_dice:, boost_dice:, setback_dice:)
+    ability_dice.times do
+      @dice_pool       << Dice.ability
+      @dice_face_count << Dice.ability.count
+    end
+
+    profiency_dice.times do
+      @dice_pool       << Dice.proficiency
+      @dice_face_count << Dice.proficiency.count
+    end
+
+    boost_dice.times do
+      @dice_pool       << Dice.boost
+      @dice_face_count << Dice.boost.count
+    end
+
+    setback_dice.times do
+      @dice_pool       << Dice.setback
+      @dice_face_count << Dice.setback.count
+    end
   end
 end
